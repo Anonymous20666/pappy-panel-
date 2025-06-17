8b4e446017afff2577738a0aa37f2a6ea25c33a4
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-06-17 21:06:07 +0530

    simplify structure

--- resources/scripts/routers/ServerRouter.tsx
+++ resources/scripts/routers/ServerRouter.tsx
@@ -20,7 +20,33 @@ import { useLocation } from 'react-router';
 import ConflictStateRenderer from '@/components/server/ConflictStateRenderer';
 import PermissionRoute from '@/components/elements/PermissionRoute';
 import routes from '@/routers/routes';
-import { Navigation, ComponentLoader } from '@/routers/ServerElements';
+
+interface Props {
+    route: any;
+}
+
+const NavItem = ({ route }: Props) => {
+    const match = useRouteMatch<{ id: string }>();
+
+    const nestId = ServerContext.useStoreState((state) => state.server.data?.nestId);
+    const eggId = ServerContext.useStoreState((state) => state.server.data?.eggId);
+
+    const to = (value: string, url = false) => {
+        return `${(url ? match.url : match.path).replace(/\/*$/, '')}/${value.replace(/^\/+/, '')}`;
+    };
+
+    return (
+        ((route.nestIds && route.nestIds.includes(nestId ?? 0)) ||
+            (route.eggIds && route.eggIds.includes(eggId ?? 0)) ||
+            (route.nestId && route.nestId === nestId) ||
+            (route.eggId && route.eggId === eggId) ||
+            (!route.eggIds && !route.nestIds && !route.nestId && !route.eggId)) && (
+            <NavLink id={route.name} to={to(route.path, true)} exact={route.exact}>
+                {route.name}
+            </NavLink>
+        )
+    );
+};
 
 export default () => {
     const match = useRouteMatch<{ id: string }>();
@@ -36,6 +62,9 @@ export default () => {
     const getServer = ServerContext.useStoreActions((actions) => actions.server.getServer);
     const clearServerState = ServerContext.useStoreActions((actions) => actions.clearServerState);
 
+    const serverNestId = ServerContext.useStoreState((state) => state.server.data?.nestId);
+    const serverEggId = ServerContext.useStoreState((state) => state.server.data?.eggId);
+    
     const to = (value: string, url = false) => {
         if (value === '/') {
             return url ? match.url : match.path;
@@ -77,7 +106,19 @@ export default () => {
                     <CSSTransition timeout={150} classNames={'fade'} appear in>
                         <SubNavigation>
                             <div>
-                                <Navigation />
+                                {routes.server
+                                    .filter((route) => !!route.name)
+                                    .map((route) =>
+                                 route.permission ? (
+                                    <Can key={route.path} action={route.permission} matchAny>
+                                        <NavItem route={route} />
+                                    </Can>
+                                  ) : (
+                                    <React.Fragment key={route.path}>
+                                        <NavItem route={route} />
+                                    </React.Fragment>
+                                  )
+                                )}
                                 {rootAdmin && (
                                     // eslint-disable-next-line react/jsx-no-target-blank
                                     <a href={`/admin/servers/view/${serverId}`} target={'_blank'}>
@@ -94,7 +135,26 @@ export default () => {
                         <ConflictStateRenderer />
                     ) : (
                         <ErrorBoundary>
-                            <ComponentLoader />
+                            <TransitionRouter>
+                                <Switch location={location}>
+                                    {routes.server.map(({ path, permission, component: Component, nestIds, eggIds, nestId, eggId }) => {
+                                        return (
+                                            ((nestIds && nestIds.includes(serverNestId ?? 0)) ||
+                                            (eggIds && eggIds.includes(serverEggId ?? 0)) ||
+                                            (nestId && serverNestId === nestId) ||
+                                            (eggId && serverEggId === eggId) ||
+                                            (!eggIds && !nestIds && !nestId && !eggId)) && (
+                                            <PermissionRoute key={path} permission={permission} path={to(path)} exact>
+                                                <Spinner.Suspense>
+                                                    <Component />
+                                                </Spinner.Suspense>
+                                            </PermissionRoute>
+                                        )
+                                    );
+                                    })}
+                                <Route path={'*'} component={NotFound} />
+                                </Switch>
+                            </TransitionRouter>
                         </ErrorBoundary>
                     )}
                 </>

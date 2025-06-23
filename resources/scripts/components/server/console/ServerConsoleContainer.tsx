724f933f00588d838dcd30a696e23a828709d495
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-06-23 18:42:44 +0530

    fix: ServerDetailsBlock styling

--- resources/scripts/components/server/console/ServerConsoleContainer.tsx
+++ resources/scripts/components/server/console/ServerConsoleContainer.tsx
@@ -37,7 +37,7 @@ const ServerConsoleContainer = () => {
                         <Console />
                     </Spinner.Suspense>
             </div>
-            <div className={'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'}>
+            <div className={'mt-2'}>
                 <Spinner.Suspense>
                     <ServerDetailsBlock />
                 </Spinner.Suspense>
--- resources/scripts/components/server/console/ServerDetailsBlock.tsx
+++ resources/scripts/components/server/console/ServerDetailsBlock.tsx
@@ -89,7 +89,7 @@ const ServerDetailsBlock = ({ className }: { className?: string }) => {
     });
 
     return (
-        <div className={className}>
+        <div className={classNames('grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4', className)}>
             <StatBlock
                 icon={faClock}
                 title={'Uptime'}

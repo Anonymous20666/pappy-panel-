480921a16416b705e3b9bfd253dff0170cbbc41c
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-06-26 18:41:02 +0530

    revamp: Make navbar transparent and use styled div for container

--- resources/scripts/components/ui/Navbar.tsx
+++ resources/scripts/components/ui/Navbar.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import SearchContainer from '@/components/dashboard/search/SearchContainer';
+import styled from 'styled-components/macro';
+import tw from 'twin.macro';
 
 interface NavbarProps {
     children: React.ReactChild | React.ReactFragment | React.ReactPortal;
 }
+const Container = styled.div`
+    ${tw`fixed top-0 left-0 w-full h-16 z-50`}
+`;
 
 export default ({ children }: NavbarProps) => {
   return (
-    <header className="fixed top-0 left-0 w-full h-16 bg-gray-700 border-b border-gray-600 shadow backdrop-blur-lg z-50">
-      <div className="max-w-screen-2xl mx-auto flex items-center justify-between h-full px-4 sm:px-6 md:px-8">
+    <Container className="shadow backdrop-blur-lg">
+      <div className="max-w-screen-2xl mt-1 mx-auto flex items-center justify-between h-full px-4 sm:px-6 md:px-8">
         
         <div className="flex items-center gap-4">
           {children}
@@ -17,7 +22,7 @@ export default ({ children }: NavbarProps) => {
         <SearchContainer />
       </div>
       </div>
-    </header>
+    </Container>
   );
 };
 

34af8e2c0998515336562a1f0cabd4c88dc92dae
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-06-26 18:29:04 +0530

    add spacing between content and copyright

--- resources/scripts/components/elements/PageContentBlock.tsx
+++ resources/scripts/components/elements/PageContentBlock.tsx
@@ -24,7 +24,7 @@ const PageContentBlock: React.FC<PageContentBlockProps> = ({ title, showFlashKey
                     {showFlashKey && <FlashMessageRender byKey={showFlashKey} css={tw`mb-4`} />}
                     {children}
                 </ContentContainer>
-                <ContentContainer css={tw`mb-4`}>
+                <ContentContainer css={tw`mt-4 mb-4`}>
                     <p css={tw`text-center text-neutral-500 text-xs`}>
                         <a
                             rel={'noopener nofollow noreferrer'}

b9eac7ef4312d092006b19d3dc643ca85585f336
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-07-06 12:19:41 +0530

    add meta to wrapper

--- resources/views/templates/wrapper.blade.php
+++ resources/views/templates/wrapper.blade.php
@@ -9,14 +9,15 @@
             <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
             <meta name="csrf-token" content="{{ csrf_token() }}">
             <meta name="robots" content="noindex">
-            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-            <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
-            <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
-            <link rel="manifest" href="/favicons/manifest.json">
-            <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
-            <link rel="shortcut icon" href="/favicons/favicon.ico">
-            <meta name="msapplication-config" content="/favicons/browserconfig.xml">
-            <meta name="theme-color" content="#0e4688">
+            <meta name="title" content="{{ $revixConfiguration['site_title'] }}" />
+            <meta name="description" content="{{ $revixConfiguration['site_description'] }}" />
+            <link rel="icon" type="image/x-icon" href="{{ $revixConfiguration['site_favicon'] }}">
+            <meta name="theme-color" content="{{ $revixConfiguration['site_color'] }}"/>
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="{{config('app.url', 'https://localhost')}}" />
+            <meta property="og:title" content="{{ $revixConfiguration['site_title'] }}" />
+            <meta property="og:description" content="{{ $revixConfiguration['site_description'] }}" />
+            <meta property="og:image" content="{{ $revixConfiguration['site_image'] }}" />
         @show
 
         @section('user-data')
@@ -36,6 +37,21 @@
                 </script>
             @endif
         @show
+@php
+function revix($hex) {
+    $hex = str_replace('#', '', $hex);
+    if (strlen($hex) === 3) {
+        $r = hexdec(str_repeat($hex[0], 2));
+        $g = hexdec(str_repeat($hex[1], 2));
+        $b = hexdec(str_repeat($hex[2], 2));
+    } else {
+        $r = hexdec(substr($hex, 0, 2));
+        $g = hexdec(substr($hex, 2, 2));
+        $b = hexdec(substr($hex, 4, 2));
+    }
+    return "$r $g $b";
+}
+@endphp
         <style>
             @import url('//fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap');
             @import url('//fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:500&display=swap');

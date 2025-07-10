8aff705f81f4144842287ee73f08ca71f069ccb1
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-07-10 17:19:36 +0530

    fix: Dummy value isn't replaced by actual value for logo

--- resources/scripts/components/auth/LoginFormContainer.tsx
+++ resources/scripts/components/auth/LoginFormContainer.tsx
@@ -6,6 +6,8 @@ import Card from '@/components/ui/Card'
 import Title from '@/components/ui/Title';
 import { LogoContainer } from '@/components/ui/LogoContainer';
 import tw from 'twin.macro';
+import { ApplicationStore } from '@/state';
+import { useStoreState } from 'easy-peasy';
 
 type Props = React.DetailedHTMLProps<React.FormHTMLAttributes<HTMLFormElement>, HTMLFormElement> & {
     title?: string;
@@ -19,13 +21,15 @@ const CardContainer = styled.div`
     ${tw`max-w-[28.125rem] w-screen p-5`}
 `;
 
-export default forwardRef<HTMLFormElement, Props>(({ title, ...props }, ref) => (
+export default forwardRef<HTMLFormElement, Props>(({ title, ...props }, ref) => {
+    const logo = useStoreState((state: ApplicationStore) => state.revix.data!.logo);
+    return (
     <Container>
         <FlashMessageRender css={tw`mb-2 px-1`} />
         <Form {...props} ref={ref}>
           <CardContainer>
             <LogoContainer>
-             <img src="/revix/logo.png" alt="revix" css={tw`h-[3rem]`} />
+             <img src={logo} alt="revix" css={tw`h-[3rem]`} />
             </LogoContainer>
             <Card>
                 {title && <Title className="text-3xl text-center pb-3">{title}</Title>}
@@ -45,4 +49,5 @@ export default forwardRef<HTMLFormElement, Props>(({ title, ...props }, ref) => 
             </a>
         </p>
     </Container>
-));
+    )
+});

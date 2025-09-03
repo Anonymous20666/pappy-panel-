aef773497d8b11e5cd707c586cdb8d468d873fb5
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-09-03 21:04:59 +0530

    style: revamp UI for `ErrorBoundary`

--- resources/scripts/components/elements/ErrorBoundary.tsx
+++ resources/scripts/components/elements/ErrorBoundary.tsx
@@ -24,7 +24,7 @@ class ErrorBoundary extends React.Component<{}, State> {
     render() {
         return this.state.hasError ? (
             <div css={tw`flex items-center justify-center w-full my-4`}>
-                <div css={tw`flex items-center bg-neutral-900 rounded p-3 text-red-500`}>
+                <div css={tw`flex items-center bg-gray-800 border border-gray-600 rounded-ui p-3 text-red-500`}>
                     <Icon icon={faExclamationTriangle} css={tw`h-4 w-auto mr-2`} />
                     <p css={tw`text-sm text-neutral-100`}>
                         An error was encountered by the application while rendering this view. Try refreshing the page.

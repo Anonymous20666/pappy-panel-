2964381b52e6e87c3cd0cb67ce2ff3d22cbc565b
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-09-22 07:45:21 +0530

    chore(translate): `pt` translation for dashboard

--- resources/lang/pt/dashboard/index.php
+++ resources/lang/pt/dashboard/index.php
@@ -1,8 +1,20 @@
 <?php
 
 return [
-    'search' => 'Pesquisar por servidores...',
-    'no_matches' => 'Nenhum servidor foi encontrado com os critérios de pesquisa fornecidos.',
-    'cpu_title' => 'CPU',
-    'memory_title' => 'Memória',
+    'title' => 'Painel',
+    'other-servers' => 'Mostrando servidores de outros',
+    'your-servers' => 'Mostrando seus servidores',
+    'no-servers' => 'Não há servidores associados à sua conta.',
+    'no-other-servers' => 'Não há outros servidores para exibir.',
+    'server' => [
+        'online' => 'Online',
+        'offline' => 'Offline',
+        'starting' => 'Iniciando',
+        'stopping' => 'Parando',
+        'suspended' => 'Suspenso',
+        'connection-error' => 'Erro de conexão',
+        'transferring' => 'Transferindo',
+        'installing' => 'Instalando',
+        'unavailable' => 'Indisponível',
+    ],
 ];

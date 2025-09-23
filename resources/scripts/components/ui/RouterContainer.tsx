15cebe45e790f8d990011ce7a041d54a250fc56f
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-09-23 15:50:06 +0530

    feat!: implement background image

--- resources/scripts/components/ui/RouterContainer.tsx
+++ resources/scripts/components/ui/RouterContainer.tsx
@@ -2,5 +2,7 @@ import styled from 'styled-components/macro';
 import tw from 'twin.macro';
 
 export const RouterContainer = styled.div`
-    ${tw`min-h-screen h-full bg-gray-800`}
+    ${tw`min-h-screen h-full bg-gray-800 bg-fixed bg-center bg-no-repeat`}
+    background-image: var(--background);
+    background-size: cover;
 `;

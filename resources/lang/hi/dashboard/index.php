2a2c157a9af392dda87c5749aa68d6d0af547487
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-06 20:10:34 +0530

    New translations index.php (Hindi)

--- resources/lang/hi/dashboard/index.php
+++ resources/lang/hi/dashboard/index.php
@@ -6,6 +6,12 @@ return [
     'your-servers' => 'आपके सर्वरों को दिखा रहे हैं',
     'no-servers' => 'आपके खाते से जुड़े कोई सर्वर नहीं हैं।',
     'no-other-servers' => 'दिखाने के लिए कोई अन्य सर्वर नहीं हैं।',
+    'search' => [
+        'label' => 'Find Servers...',
+        'string-min' => 'Please enter at least three characters to begin searching.',
+        'form-label' => 'Search term',
+        'form-description' => 'Enter a server name, uuid, or allocation to begin searching.',
+    ],
     'server' => [
         'online' => 'ऑनलाइन',
         'offline' => 'ऑफ़लाइन',

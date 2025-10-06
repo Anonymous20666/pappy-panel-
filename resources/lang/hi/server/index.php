8808dab769d35e57e4f8fb7c7633368f7563e363
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-06 20:10:37 +0530

    New translations index.php (Hindi)

--- resources/lang/hi/server/index.php
+++ resources/lang/hi/server/index.php
@@ -4,4 +4,11 @@ return [
     'control' => 'नियंत्रण',
     'management' => 'प्रबंधन',
     'administration' => 'प्रशासन',
+    'start' => 'Start',
+    'stop' => 'Stop',
+    'restart' => 'Restart',
+    'kill' => 'Kill',
+    'kill-warning' => 'Forcibly stopping a server can lead to data corruption.',
+    'hide-stats' => 'Hide Stats',
+    'show-stats' => 'Show Stats',
 ];

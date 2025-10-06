9be8a1fbc6e664ee76023245a3198a468eb81ef1
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-06 20:10:32 +0530

    New translations index.php (Portuguese, Brazilian)

--- resources/lang/pt/dashboard/index.php
+++ resources/lang/pt/dashboard/index.php
@@ -6,6 +6,12 @@ return [
     'your-servers' => 'Mostrando seus servidores',
     'no-servers' => 'Não há servidores associados à sua conta.',
     'no-other-servers' => 'Não há outros servidores para exibir.',
+    'search' => [
+        'label' => 'Find Servers...',
+        'string-min' => 'Please enter at least three characters to begin searching.',
+        'form-label' => 'Search term',
+        'form-description' => 'Enter a server name, uuid, or allocation to begin searching.',
+    ],
     'server' => [
         'online' => 'Online',
         'offline' => 'Offline',

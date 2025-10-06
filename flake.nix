0be1bbc9768bfb92dafceffbee035d99acf64feb
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-06 13:47:32 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Pterodactyl Panel";
+  description = "Reviactyl Panel";
 
   inputs = {
     flake-parts = {
@@ -59,7 +59,7 @@
         };
 
         packages.development = pkgs.dockerTools.buildImage {
-          name = "pterodactyl/development";
+          name = "reviactyl/development";
           tag = "panel";
 
           copyToRoot = pkgs.buildEnv (let

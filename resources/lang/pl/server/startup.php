3bc095c3c18358741440f082a2b7becb0f9c016d
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-08 03:49:15 +0530

    New translations startup.php (Polish)

--- resources/lang/pl/server/startup.php
+++ resources/lang/pl/server/startup.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'title' => 'Startup',
-    'startup-command' => 'Startup Command',
-    'docker-image' => 'Docker Image',
-    'docker-info' => 'This is an advanced feature allowing you to select a Docker image to use when running this server instance.',
-    'manually-set-docker' => 'This server\'s Docker image has been manually set by an administrator and cannot be changed through this UI.',
-    'variables' => 'Variables',
+    'title' => 'Uruchamianie',
+    'startup-command' => 'Komenda uruchamiania',
+    'docker-image' => 'Obraz Docker',
+    'docker-info' => 'Jest to zaawansowana funkcja umożliwiająca wybór obrazu Docker do użycia podczas uruchamiania tej instancji serwera.',
+    'manually-set-docker' => 'Obraz Docker tego serwera został ręcznie ustawiony przez administratora i nie może być zmieniony za pośrednictwem tego interfejsu użytkownika',
+    'variables' => 'Zmienne',
 ];

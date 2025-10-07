a291f0e6123a3e7fe0495f39dec089904a4a1c9e
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-08 03:49:10 +0530

    New translations files.php (Polish)

--- resources/lang/pl/server/files.php
+++ resources/lang/pl/server/files.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'title' => 'File Manager',
-    'new-file' => 'New File',
-    'empty' => 'This directory seems to be empty.',
-    'too-large' => 'This directory is too large to display in the browser, limiting the output to the first 250 files.',
-    'move' => 'Move',
-    'archive' => 'Archive',
-    'delete' => 'Delete',
-    'create-directory' => 'Create Directory',
+    'title' => ' Menedżer plików',
+    'new-file' => 'Nowy plik',
+    'empty' => 'Ten katalog wydaje się być pusty.',
+    'too-large' => 'Ten katalog jest zbyt duży, aby wyświetlić go w przeglądarce, ograniczając wynik do pierwszych 250 plików.',
+    'move' => 'Przenieś',
+    'archive' => 'Archiwizuj',
+    'delete' => 'Usuń',
+    'create-directory' => 'Utwórz katalog',
 ];

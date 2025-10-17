ad979e83c4488458b80c0e81bd2f721b3c8265f8
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-17 21:55:33 +0530

    New translations network.php (Chinese Simplified)

--- resources/lang/zh/server/network.php
+++ resources/lang/zh/server/network.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'Network',
-    'created' => 'You are currently using {{count}} of {{limit}} allowed allocations for this server.',
-    'create' => 'Create Allocation',
+    'title' => '网络',
+    'created' => '您当前已使用 {{count}} 个分配项，共可使用 {{limit}} 个。',
+    'create' => '创建分配',
 ];

c4984bda6bbb26ee859845cd949d7bbdc7af1690
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-17 21:55:34 +0530

    New translations schedules.php (Chinese Simplified)

--- resources/lang/zh/server/schedules.php
+++ resources/lang/zh/server/schedules.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'Schedules',
-    'no-schedules' => 'There are no schedules configured for this server.',
-    'create-schedule' => 'Create Schedule',
+    'title' => '计划',
+    'no-schedules' => '目前没有为此服务器配置计划。',
+    'create-schedule' => '创建计划',
 ];

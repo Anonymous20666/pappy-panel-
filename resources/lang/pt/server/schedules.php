511c62ca82195f6335eed312a033dc3c724aca3c
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-20 18:18:10 +0530

    New translations schedules.php (Portuguese, Brazilian)

--- resources/lang/pt/server/schedules.php
+++ resources/lang/pt/server/schedules.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'Schedules',
+    'title' => 'Agendamentos',
     'no-schedules' => 'Não existem schedules configurados para este servidor.',
     'create-schedule' => 'Criar Agendamento',
 ];

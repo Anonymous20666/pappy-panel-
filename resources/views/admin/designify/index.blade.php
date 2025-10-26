3d7916cfaaec0bd6be890b43d8d5ed960b1a55be
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-26 18:03:25 +0530

    update colors for designify

--- resources/views/admin/designify/index.blade.php
+++ resources/views/admin/designify/index.blade.php
@@ -12,13 +12,13 @@
         </div>
         <section class="mb-8">
             <div>
-                <div class="bg-orange-600/10 border border-orange-600 rounded-lg shadow">
-                    <div class="border-b border-orange-600 px-4 py-2">
-                        <h3 class="text-lg font-semibold text-orange-600">System Information</h3>
+                <div class="bg-zinc-600/10 border border-zinc-600 rounded-lg shadow">
+                    <div class="border-b border-zinc-600 px-4 py-2">
+                        <h3 class="text-lg font-semibold text-zinc-600">System Information</h3>
                     </div>
-                    <div class="px-4 py-3 text-orange-200">
+                    <div class="px-4 py-3 text-zinc-200">
                         You are running Reviactyl Panel version <code
-                            class="px-1 py-0.5 border border-orange-800 rounded text-sm">v{{ config('app.version') }}</code>.
+                            class="px-1 py-0.5 border border-zinc-800 rounded text-sm">v{{ config('app.version') }}</code>.
                     </div>
                 </div>
             </div>

d62694ff5087cf2f6ded039e7ab9e30490c543a2
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-26 18:12:27 +0530

    add SideNavigation styles inside Sidebar

--- resources/scripts/reviactyl/ui/Sidebar.tsx
+++ resources/scripts/reviactyl/ui/Sidebar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 import tw from 'twin.macro';
-import { SideNavigation } from './sidebar/SideNavigation';
 import { Link, NavLink } from 'react-router-dom';
 import Avatar from '@/reviactyl/ui/Avatar';
 import { useStoreState } from 'easy-peasy';
@@ -45,6 +44,24 @@ const ProfileHeader = styled.div`
 
 const SidebarContent = styled.div`
     ${tw`flex flex-col flex-1 overflow-y-auto`}
+`;
+
+export const SideNavigation = styled.div`
+    ${tw`flex flex-col gap-1 pb-4 -mt-1`};
+
+    & .label {
+        ${tw`flex items-center ml-2 mr-2 px-3 pt-2 pb-1 text-sm font-semibold text-gray-100 uppercase rounded-ui transition-all duration-300`};
+    }
+    a {
+        ${tw`flex items-center ml-2 mr-2 px-5 py-2 text-sm font-medium text-gray-200 rounded-ui transition-all duration-300`};
+
+        &:hover,
+        &:focus,
+        &.active {
+            ${tw`text-reviactyl`};
+            background-color: rgb(var(--color-primary) / 0.2);
+        }
+    }
 `;
 
 const Sidebar = ({ children, isOpen = false, dashboard = false }: Props) => {

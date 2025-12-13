bc64cea07f69144519cefacdf0a2ebb8f9abfe1c
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-12-13 18:23:12 +0530

    New translations startup.php (French)

--- resources/lang/fr/server/startup.php
+++ resources/lang/fr/server/startup.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'title' => 'Démarrage',
-    'startup-command' => 'Commande de démarrage',
-    'docker-image' => 'Image Docker',
-    'docker-info' => 'Il s\'agit d\'une fonctionnalité avancée qui vous permet de sélectionner une image Docker à utiliser lors de l\'exécution de cette instance de serveur.',
-    'manually-set-docker' => 'L\'image Docker de ce serveur a été configurée manuellement par un administrateur et ne peut pas être modifiée via le panel.',
+    'title' => 'Startup',
+    'startup-command' => 'Startup Command',
+    'docker-image' => 'Docker Image',
+    'docker-info' => 'This is an advanced feature allowing you to select a Docker image to use when running this server instance.',
+    'manually-set-docker' => 'This server\'s Docker image has been manually set by an administrator and cannot be changed through this UI.',
     'variables' => 'Variables',
 ];

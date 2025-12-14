2569161acbc6554b0a5a1bf0901e3954f086e17a
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-12-14 09:14:18 +0530

    New translations console.php (French)

--- resources/lang/fr/server/console.php
+++ resources/lang/fr/server/console.php
@@ -2,8 +2,8 @@
 
 return [
     'title' => 'Console',
-    'node-under-maintenance' => 'The node of this server is currently under maintenance and all actions are unavailable.',
-    'server-installing' => 'This server is currently running its installation process and most actions are unavailable.',
-    'server-transferring' => 'This server is currently being transferred to another node and all actions are unavailable.',
-    'run-command' => 'Type a command...',
+    'node-under-maintenance' => 'Le noeud de ce serveur est actuellement en maintenance et toutes les actions sont indisponibles.',
+    'server-installing' => 'Ce serveur est actuellement en cours d\'installation et la plupart des actions ne sont pas disponibles.',
+    'server-transferring' => 'Ce serveur est actuellement transféré vers un autre nœud et toutes les actions sont indisponibles.',
+    'run-command' => 'Tapez une commande...',
 ];

52e1884334a263f653f1ef25e149bafdf39bf1ef
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-09 12:00:38 +0530

    New translations passwords.php (Swedish)

--- resources/lang/sv/passwords.php
+++ resources/lang/sv/passwords.php
@@ -11,9 +11,9 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Lösenord måste vara minst sex tecken och matcha bekräftelsen.',
+    'reset' => 'Ditt lösenord har återställts!',
+    'sent' => 'Vi har skickat e-post med länk för lösenordsåterställning!',
+    'token' => 'Denna lösenordsåterställningstoken är ogiltig.',
+    'user' => "Vi kan inte hitta en användare med den e-postadressen.",
 ];

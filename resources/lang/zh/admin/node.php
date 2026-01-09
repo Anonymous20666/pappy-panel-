1042759d8b84e3c7a9fa86e57c0fc4f5c59c7d06
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-09 10:25:58 +0530

    New translations node.php (Chinese Simplified)

--- resources/lang/zh/admin/node.php
+++ resources/lang/zh/admin/node.php
@@ -2,15 +2,15 @@
 
 return [
     'validation' => [
-        'fqdn_not_resolvable' => '提供的 FQDN 或 IP 地址无法解析为有效的 IP 地址。',
-        'fqdn_required_for_ssl' => '要为此节点使用 SSL，需要一个能解析为公共 IP 地址的完全限定域名。',
+        'fqdn_not_resolvable' => 'The FQDN or IP address provided does not resolve to a valid IP address.',
+        'fqdn_required_for_ssl' => 'A fully qualified domain name that resolves to a public IP address is required in order to use SSL for this node.',
     ],
     'notices' => [
-        'allocations_added' => '分配已成功添加到此节点。',
-        'node_deleted' => '节点已成功从面板中删除。',
-        'location_required' => '在向此面板添加节点之前，您必须至少配置一个位置。',
-        'node_created' => '新节点创建成功。您可以通过访问"配置"选项卡自动配置此机器上的守护进程。<strong>在添加任何服务器之前，您必须首先分配至少一个 IP 地址和端口。</strong>',
-        'node_updated' => '节点信息已更新。如果更改了任何守护进程设置，您需要重新启动它才能使这些更改生效。',
-        'unallocated_deleted' => '已删除 <code>:ip</code> 的所有未分配端口。',
+        'allocations_added' => 'Allocations have successfully been added to this node.',
+        'node_deleted' => 'Node has been successfully removed from the panel.',
+        'location_required' => 'You must have at least one location configured before you can add a node to this panel.',
+        'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. <strong>Before you can add any servers you must first allocate at least one IP address and port.</strong>',
+        'node_updated' => 'Node information has been updated. If any daemon settings were changed you will need to reboot it for those changes to take effect.',
+        'unallocated_deleted' => 'Deleted all un-allocated ports for <code>:ip</code>.',
     ],
 ];

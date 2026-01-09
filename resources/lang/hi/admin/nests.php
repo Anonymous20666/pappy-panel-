fc9a955aaed17ff4b45ab9ac72936ad11865afce
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-09 11:59:50 +0530

    New translations nests.php (Hindi)

--- resources/lang/hi/admin/nests.php
+++ resources/lang/hi/admin/nests.php
@@ -2,25 +2,25 @@
 
 return [
     'notices' => [
-        'created' => 'A new nest, :name, has been successfully created.',
-        'deleted' => 'Successfully deleted the requested nest from the Panel.',
-        'updated' => 'Successfully updated the nest configuration options.',
+        'created' => 'एक नया नेस्ट, :name, सफलतापूर्वक बनाया गया है।',
+        'deleted' => 'पैनल से अनुरोधित नेस्ट सफलतापूर्वक हटा दिया गया है।',
+        'updated' => 'नेस्ट कॉन्फ़िगरेशन विकल्प सफलतापूर्वक अपडेट किए गए हैं।',
     ],
     'eggs' => [
         'notices' => [
-            'imported' => 'Successfully imported this Egg and its associated variables.',
-            'updated_via_import' => 'This Egg has been updated using the file provided.',
-            'deleted' => 'Successfully deleted the requested egg from the Panel.',
-            'updated' => 'Egg configuration has been updated successfully.',
-            'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
-            'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
+            'imported' => 'इस Egg और इससे जुड़े वेरिएबल्स को सफलतापूर्वक आयात किया गया।',
+            'updated_via_import' => 'इस Egg को दी गई फ़ाइल का उपयोग करके अपडेट किया गया है।',
+            'deleted' => 'पैनल से अनुरोधित egg सफलतापूर्वक हटा दिया गया है।',
+            'updated' => 'Egg कॉन्फ़िगरेशन सफलतापूर्वक अपडेट किया गया है।',
+            'script_updated' => 'Egg इंस्टॉल स्क्रिप्ट अपडेट कर दी गई है और जब भी सर्वर इंस्टॉल होंगे तब चलेगी।',
+            'egg_created' => 'एक नया egg सफलतापूर्वक बनाया गया। इस नए egg को लागू करने के लिए आपको किसी भी चल रहे डेमन को पुनः आरंभ करना होगा।',
         ],
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
-            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
-            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
+            'variable_deleted' => 'वेरिएबल ":variable" हटा दिया गया है और एक बार पुनर्निर्माण के बाद सर्वरों के लिए उपलब्ध नहीं होगा।',
+            'variable_updated' => 'वेरिएबल ":variable" अपडेट किया गया है। परिवर्तन लागू करने के लिए आपको इस वेरिएबल का उपयोग करने वाले किसी भी सर्वर को पुनर्निर्माण करना होगा।',
+            'variable_created' => 'नया वेरिएबल सफलतापूर्वक बनाया गया और इस egg को असाइन किया गया।',
         ],
     ],
 ];

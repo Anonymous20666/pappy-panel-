e7d794ff30df5673d590fd54ccaf6260376220e3
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-09 12:00:09 +0530

    New translations user.php (Swedish)

--- resources/lang/sv/admin/user.php
+++ resources/lang/sv/admin/user.php
@@ -3,10 +3,10 @@
 return [
     'exceptions' => [
         'delete_self' => 'You cannot delete your own account.',
-        'user_has_servers' => 'Cannot delete a user with active servers attached to their account. Please delete their servers before continuing.',
+        'user_has_servers' => 'Kan inte ta bort en användare med aktiva servrar kopplade till deras konto. Vänligen ta bort deras servrar innan du fortsätter.',
     ],
     'notices' => [
-        'account_created' => 'Account has been created successfully.',
-        'account_updated' => 'Account has been successfully updated.',
+        'account_created' => 'Kontot har skapats framgångsrikt.',
+        'account_updated' => 'Kontot har uppdaterats framgångsrikt.',
     ],
 ];

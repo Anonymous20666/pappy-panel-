efcd1083346abad3866345a04342c920f6636a63
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-09 10:25:53 +0530

    New translations nests.php (Indonesian)

--- resources/lang/id/admin/nests.php
+++ resources/lang/id/admin/nests.php
@@ -2,25 +2,25 @@
 
 return [
     'notices' => [
-        'created' => 'Sebuah nest baru, :name, telah berhasil dibuat.',
-        'deleted' => 'Berhasil menghapus nest yang diminta dari Panel.',
-        'updated' => 'Berhasil memperbarui opsi konfigurasi nest.',
+        'created' => 'A new nest, :name, has been successfully created.',
+        'deleted' => 'Successfully deleted the requested nest from the Panel.',
+        'updated' => 'Successfully updated the nest configuration options.',
     ],
     'eggs' => [
         'notices' => [
-            'imported' => 'Berhasil mengimpor Egg ini dan variabel terkaitnya.',
-            'updated_via_import' => 'Egg ini telah diperbarui menggunakan file yang disediakan.',
-            'deleted' => 'Berhasil menghapus egg yang diminta dari Panel.',
-            'updated' => 'Konfigurasi Egg telah berhasil diperbarui.',
-            'script_updated' => 'Skrip instal egg telah diperbarui dan akan berjalan setiap kali server diinstal.',
-            'egg_created' => 'Sebuah egg baru berhasil dibuat. Anda perlu merestart daemon yang berjalan untuk menerapkan egg baru ini.',
+            'imported' => 'Successfully imported this Egg and its associated variables.',
+            'updated_via_import' => 'This Egg has been updated using the file provided.',
+            'deleted' => 'Successfully deleted the requested egg from the Panel.',
+            'updated' => 'Egg configuration has been updated successfully.',
+            'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
+            'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
         ],
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'Variabel ":variable" telah dihapus dan tidak akan tersedia lagi untuk server setelah rebuild.',
-            'variable_updated' => 'Variabel ":variable" telah diperbarui. Anda perlu me-rebuild server yang menggunakan variabel ini untuk menerapkan perubahan.',
-            'variable_created' => 'Variabel baru berhasil dibuat dan ditugaskan ke egg ini.',
+            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
+            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
+            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
         ],
     ],
 ];

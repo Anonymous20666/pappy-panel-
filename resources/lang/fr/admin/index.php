a997bc0074017b11896a22cd42b9efafdd8be1f7
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-15 21:26:20 +0530

    New translations index.php (French)

--- resources/lang/fr/admin/index.php
+++ resources/lang/fr/admin/index.php
@@ -3,9 +3,9 @@
 return [
     'title' => 'Dashboard',
     'uptodate-header' => 'Tout est à jour',
-    'uptodate-body' => 'Vous utilisez la version <code>:version</code>. Tout est à jour!',
+    'uptodate-body' => 'You are currently using :version. Your panel is up-to-date!',
     'notuptodate-header' => 'Le panel n\'est pas à jour!',
-    'notuptodate-body' => '<code>:latest</code> est maintenant disponible! Allez lire notre documentation pour mettre à jour le panel.',
+    'notuptodate-body' => ':latest is now available! Read our documentation to update your Panel.',
     'feedback-header' => 'Un avis?',
     'feedback-body' => 'Si vous appréciez les fonctionnalités et la facilité d\'utilisation de Reviactyl, faites-le-nous savoir ! Vos commentaires nous aident à améliorer et à créer des outils encore plus performants pour vous.',
     'feedback-btn' => 'Faire un retour',

b37b488f3c28496ac51d41f29328272ac6013d13
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-15 21:26:21 +0530

    New translations index.php (Spanish)

--- resources/lang/es/admin/index.php
+++ resources/lang/es/admin/index.php
@@ -3,9 +3,9 @@
 return [
     'title' => 'Panel de control',
     'uptodate-header' => 'Tu panel está actualizado',
-    'uptodate-body' => 'Actualmente estás utilizando <code>:version</code>. ¡Tu panel está actualizado!',
+    'uptodate-body' => 'You are currently using :version. Your panel is up-to-date!',
     'notuptodate-header' => 'Tu panel no está actualizado',
-    'notuptodate-body' => '¡Ya disponible una <code>:latest</code> actualización! Lea nuestra documentación para actualizar su Panel.',
+    'notuptodate-body' => ':latest is now available! Read our documentation to update your Panel.',
     'feedback-header' => '¿Algún comentario?',
     'feedback-body' => 'Si has disfrutado de las funciones y la facilidad de uso que ofrece Reviactyl, ¡cuéntanoslo! Tus comentarios nos ayudan a mejorar y a crear herramientas aún más increíbles para ti.',
     'feedback-btn' => 'Crear incidencia',

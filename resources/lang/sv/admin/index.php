d18f6881fd74b59a0d51aacf5b47973361f2daa4
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-15 21:26:22 +0530

    New translations index.php (Swedish)

--- resources/lang/sv/admin/index.php
+++ resources/lang/sv/admin/index.php
@@ -3,9 +3,9 @@
 return [
     'title' => 'Instrumentpanel',
     'uptodate-header' => 'Din panel är uppdaterad',
-    'uptodate-body' => 'Du använder för närvarande <code>:version</code>. Din panel är uppdaterad!',
+    'uptodate-body' => 'You are currently using :version. Your panel is up-to-date!',
     'notuptodate-header' => 'Din panel är inte uppdaterad',
-    'notuptodate-body' => '<code>:latest</code> är nu tillgänglig! Läs vår dokumentation för att uppdatera din panel.',
+    'notuptodate-body' => ':latest is now available! Read our documentation to update your Panel.',
     'feedback-header' => 'Någon feedback?',
     'feedback-body' => 'Om du har uppskattat funktionerna och användarvänligheten som Reviactyl erbjuder, låt oss veta! Din feedback hjälper oss att förbättra och skapa ännu bättre verktyg för dig.',
     'feedback-btn' => 'Skapa ärende',

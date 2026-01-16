3fc1a9b1a3b3e2c61043b7e93346c814561b1efe
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-01-16 09:08:05 +0530

    dashboard page finish!

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -6,17 +6,12 @@ use Filament\Pages\Dashboard as BaseDashboard;
 
 class Dashboard extends BaseDashboard
 {
-    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-home';
+    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-squares-2x2';
 
-    protected static string|\BackedEnum|null $activeNavigationIcon = 'heroicon-s-home';
+    protected static string|\BackedEnum|null $activeNavigationIcon = 'heroicon-s-squares-2x2';
 
     public function getHeading(): string
     {
         return trans('admin/index.title');
-    }
-
-    public function getColumns(): int|array
-    {
-        return 1;
     }
 }

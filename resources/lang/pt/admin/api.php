ae12048c97fc4871737c12f2f76eb66bb7fe25d9
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-18 12:47:52 +0530

    New translations api.php (Portuguese, Brazilian)

--- resources/lang/pt/admin/api.php
+++ resources/lang/pt/admin/api.php
@@ -7,11 +7,17 @@ return [
     'memo' => 'Observação',
     'last-used' => 'Último uso',
     'created' => 'Criado em',
+    'never-used' => 'Never Used',
+
+    'revoke' => 'Revoke',
+    'revoke-title' => 'Revoke API Key',
+    'revoke-warning' => 'Once this API key is revoked any applications currently using it will stop working.',
+    'revoked' => 'API Key has been revoked.',
 
     'permissions' => 'Selecionar permissões',
     'read-only' => 'Leitura',
-    'read-write' => 'Leitura &amp; Escrita',
-    'none' => 'nenhum',
+    'read-write' => 'Read & Write',
+    'none' => 'None',
 
     'description' => 'Descrição',
     'info' => 'Depois de atribuir permissões e criar este conjunto de credenciais, você não poderá voltar para editá-lo. Se precisar fazer alterações no futuro, será necessário criar um novo conjunto de credenciais.',

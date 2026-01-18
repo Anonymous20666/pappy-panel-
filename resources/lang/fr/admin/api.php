88bdace027fcf8cb0c086545b791b4e79ca22cf4
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-18 12:47:49 +0530

    New translations api.php (French)

--- resources/lang/fr/admin/api.php
+++ resources/lang/fr/admin/api.php
@@ -7,11 +7,17 @@ return [
     'memo' => 'Mémo',
     'last-used' => 'Dernière utilisation',
     'created' => 'Créé le',
+    'never-used' => 'Never Used',
+
+    'revoke' => 'Revoke',
+    'revoke-title' => 'Revoke API Key',
+    'revoke-warning' => 'Once this API key is revoked any applications currently using it will stop working.',
+    'revoked' => 'API Key has been revoked.',
 
     'permissions' => 'Sélectionner les permissions',
     'read-only' => 'Lecture',
-    'read-write' => 'Lecture &amp; écriture',
-    'none' => 'aucun',
+    'read-write' => 'Read & Write',
+    'none' => 'None',
 
     'description' => 'Description',
     'info' => 'Une fois que vous avez attribué des permissions et créé cet ensemble d’identifiants, vous ne pourrez plus revenir en arrière pour le modifier. Si vous devez effectuer des changements ultérieurement, vous devrez créer un nouvel ensemble d’identifiants.',

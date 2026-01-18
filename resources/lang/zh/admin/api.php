dd9466b8ebc62900e5b9812b0070f2ad137e7325
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-18 12:47:48 +0530

    New translations api.php (Chinese Simplified)

--- resources/lang/zh/admin/api.php
+++ resources/lang/zh/admin/api.php
@@ -7,11 +7,17 @@ return [
     'memo' => '备注',
     'last-used' => '最后使用',
     'created' => '创建于',
+    'never-used' => 'Never Used',
+
+    'revoke' => 'Revoke',
+    'revoke-title' => 'Revoke API Key',
+    'revoke-warning' => 'Once this API key is revoked any applications currently using it will stop working.',
+    'revoked' => 'API Key has been revoked.',
 
     'permissions' => '选择权限',
     'read-only' => '只读',
-    'read-write' => '读写',
-    'none' => '无',
+    'read-write' => 'Read & Write',
+    'none' => 'None',
 
     'description' => '描述',
     'info' => '一旦分配权限并创建了这组凭据，您将无法返回进行编辑。如需在之后进行更改，必须创建一组新的凭据。',

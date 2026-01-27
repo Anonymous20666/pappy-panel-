e0d75e0c6bc84ff98e5192f4e05053a1593b71f8
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-27 18:41:24 +0530

    New translations auth.php (Indonesian)

--- resources/lang/id/auth.php
+++ resources/lang/id/auth.php
@@ -13,6 +13,14 @@ return [
     'login-button' => 'Masuk',
     'return' => 'Kembali ke Login',
 
+    'social' => [
+        'or' => 'OR',
+        'google' => 'Google',
+        'discord' => 'Discord',
+        'github' => 'GitHub',
+        'not_linked' => 'This account has not been linked to any :provider account. Please log in with your email and password first, then link your :provider account in the Account Settings page.',
+    ],
+
     'forgot-password' => [
         'title' => 'Minta Atur Ulang Kata Sandi',
         'label' => 'Lupa Kata Sandi?',

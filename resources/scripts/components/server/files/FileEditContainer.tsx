6fdf944a2ea579016bfbd873af2544fdd008cb04
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2026-02-07 02:56:52 +0100

    Implement Monaco Editor

--- resources/scripts/components/server/files/FileEditContainer.tsx
+++ resources/scripts/components/server/files/FileEditContainer.tsx
@@ -20,7 +20,11 @@ import ErrorBoundary from '@/components/elements/ErrorBoundary';
 import { encodePathSegments, hashToPath } from '@/helpers';
 import { dirname } from 'pathe';
 import CodemirrorEditor from '@/components/elements/CodemirrorEditor';
+import MonacoEditor from '@/components/elements/MonacoEditor';
 import Card from '@/reviactyl/ui/Card';
+
+import { ApplicationStore } from '@/state';
+import { useStoreState } from 'easy-peasy';
 
 export default () => {
     const [error, setError] = useState('');
@@ -36,6 +40,8 @@ export default () => {
     const id = ServerContext.useStoreState((state) => state.server.data!.id);
     const uuid = ServerContext.useStoreState((state) => state.server.data!.uuid);
     const setDirectory = ServerContext.useStoreActions((actions) => actions.files.setDirectory);
+    const user = useStoreState((state: ApplicationStore) => state.user.data);
+    
     const { addError, clearFlashes } = useFlash();
 
     let fetchFileContent: null | (() => Promise<string>) = null;
@@ -120,22 +126,42 @@ export default () => {
             />
             <Card css={tw`relative !p-1 !rounded-none mb-1`}>
                 <SpinnerOverlay visible={loading} />
-                <CodemirrorEditor
-                    mode={mode}
-                    filename={hash.replace(/^#/, '')}
-                    onModeChanged={setMode}
-                    initialContent={content}
-                    fetchContent={(value) => {
-                        fetchFileContent = value;
-                    }}
-                    onContentSaved={() => {
-                        if (action !== 'edit') {
-                            setModalVisible(true);
-                        } else {
-                            save();
-                        }
-                    }}
-                />
+                {user?.fileEditor === 'cm' && (
+                    <CodemirrorEditor
+                        mode={mode}
+                        filename={hash.replace(/^#/, '')}
+                        onModeChanged={setMode}
+                        initialContent={content}
+                        fetchContent={(value) => {
+                            fetchFileContent = value;
+                        }}
+                        onContentSaved={() => {
+                            if (action !== 'edit') {
+                                setModalVisible(true);
+                            } else {
+                                save();
+                            }
+                        }}
+                    />
+                )}
+                {user?.fileEditor === 'mo' && (
+                    <MonacoEditor
+                        mode={mode}
+                        filename={hash.replace(/^#/, '')}
+                        onModeChanged={setMode}
+                        initialContent={content}
+                        fetchContent={(value) => {
+                            fetchFileContent = value;
+                        }}
+                        onContentSaved={() => {
+                            if (action !== 'edit') {
+                                setModalVisible(true);
+                            } else {
+                                save();
+                            }
+                        }}
+                    />
+                )}
             </Card>
             <Card css={tw`flex justify-end !rounded-t-none !px-2 !py-3`}>
                 <div css={tw`flex-1 sm:flex-none rounded-ui bg-gray-700 border border-gray-600 mr-4`}>

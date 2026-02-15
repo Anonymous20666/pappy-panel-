4b0fe82cde62ab41dcf261573e27c391a038238e
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-02-15 09:18:16 +0530

    use `@hello-pangea/dnd`
    react-beautiful-dnd package is depreciated

--- resources/scripts/components/dashboard/CategoryManagerModal.tsx
+++ resources/scripts/components/dashboard/CategoryManagerModal.tsx
@@ -14,7 +14,7 @@ import FlashMessageRender from '@/components/FlashMessageRender';
 import styled from 'styled-components/macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faPen, faPlus, faLayerGroup, faSortAmountDown, faBars } from '@fortawesome/free-solid-svg-icons';
-import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable, DropResult } from '@hello-pangea/dnd';
 import { useTranslation } from 'react-i18next';
 
 interface Props {

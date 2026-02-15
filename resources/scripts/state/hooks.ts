aba6c625bfad4e2b6ac5397ca4b094e50b5ab7f0
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2026-02-15 10:24:55 +0100

    Removed redundant type declaration

--- resources/scripts/state/hooks.ts
+++ resources/scripts/state/hooks.ts
@@ -1,10 +1,9 @@
 import { createTypedHooks } from 'easy-peasy';
 import { ApplicationStore } from '@/state/index';
-import type { Dispatch } from 'redux';
 
 const hooks = createTypedHooks<ApplicationStore>();
 
 export const useStore = hooks.useStore;
 export const useStoreState = hooks.useStoreState;
 export const useStoreActions = hooks.useStoreActions;
-export const useStoreDispatch: () => Dispatch = hooks.useStoreDispatch;
+export const useStoreDispatch = hooks.useStoreDispatch;

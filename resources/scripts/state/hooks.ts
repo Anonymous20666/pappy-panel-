76178d61ef8cdb201f01fa6116580029a1938386
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2026-02-15 09:39:43 +0100

    Add explicit type annotations for pnpm dependency resolution

--- resources/scripts/state/hooks.ts
+++ resources/scripts/state/hooks.ts
@@ -1,9 +1,10 @@
 import { createTypedHooks } from 'easy-peasy';
 import { ApplicationStore } from '@/state/index';
+import type { Dispatch } from 'redux';
 
 const hooks = createTypedHooks<ApplicationStore>();
 
 export const useStore = hooks.useStore;
 export const useStoreState = hooks.useStoreState;
 export const useStoreActions = hooks.useStoreActions;
-export const useStoreDispatch = hooks.useStoreDispatch;
+export const useStoreDispatch: () => Dispatch = hooks.useStoreDispatch;

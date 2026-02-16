66ae2aaef50db039f53fd8f29f7d5e2c81f03307
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-16 10:33:38 +0530

    New translations node.php (Portuguese, Brazilian)

--- resources/lang/pt/admin/node.php
+++ resources/lang/pt/admin/node.php
@@ -190,7 +190,7 @@ return [
         'allocations_added' => 'As alocações foram adicionadas com sucesso a este nó.',
         'node_deleted' => 'O nó foi removido com sucesso do painel.',
         'location_required' => 'Você deve ter pelo menos uma localização configurada antes de poder adicionar um nó a este painel.',
-        'node_created' => 'Novo nó criado com sucesso. Você pode configurar automaticamente o daemon nesta máquina visitando a aba \'Configuração\'. <strong>Antes de poder adicionar quaisquer servidores, você deve primeiro alocar pelo menos um endereço IP e porta.</strong>',
+        'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. Before you can add any servers you must first allocate at least one IP address and port.',
         'node_updated' => 'As informações do nó foram atualizadas. Se quaisquer configurações do daemon foram alteradas, você precisará reiniciá-lo para que essas alterações entrem em vigor.',
         'unallocated_deleted' => 'Todas as portas não alocadas para <code>:ip</code> foram excluídas.',
     ],

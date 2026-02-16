8150dcdd985545d1619dc533d319d5fee65338bf
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-16 10:33:32 +0530

    New translations node.php (Swedish)

--- resources/lang/sv/admin/node.php
+++ resources/lang/sv/admin/node.php
@@ -190,7 +190,7 @@ return [
         'allocations_added' => 'Allokeringar har lagts till framgångsrikt på denna nod.',
         'node_deleted' => 'Noden har tagits bort framgångsrikt från panelen.',
         'location_required' => 'Du måste ha minst en plats konfigurerad innan du kan lägga till en nod i denna panel.',
-        'node_created' => 'Ny nod skapad framgångsrikt. Du kan automatiskt konfigurera daemonen på denna maskin genom att besöka fliken \'Konfiguration\'. <strong>Innan du kan lägga till några servrar måste du först allokera minst en IP-adress och port.</strong>',
+        'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. Before you can add any servers you must first allocate at least one IP address and port.',
         'node_updated' => 'Nodinformationen har uppdaterats. Om några daemoninställningar ändrades behöver du starta om den för att dessa ändringar ska träda i kraft.',
         'unallocated_deleted' => 'Alla icke-allokerade portar för <code>:ip</code> har tagits bort.',
     ],

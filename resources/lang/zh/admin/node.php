bf3273d897b4cc7cc885866227766f324786fb4d
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-02-16 10:33:36 +0530

    New translations node.php (Chinese Simplified)

--- resources/lang/zh/admin/node.php
+++ resources/lang/zh/admin/node.php
@@ -190,7 +190,7 @@ return [
         'allocations_added' => '分配已成功添加到此节点。',
         'node_deleted' => '节点已成功从面板中删除。',
         'location_required' => '在向此面板添加节点之前，您必须至少配置一个位置。',
-        'node_created' => '新节点创建成功。您可以通过访问"配置"选项卡自动配置此机器上的守护进程。<strong>在添加任何服务器之前，您必须首先分配至少一个 IP 地址和端口。</strong>',
+        'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. Before you can add any servers you must first allocate at least one IP address and port.',
         'node_updated' => '节点信息已更新。如果更改了任何守护进程设置，您需要重新启动它才能使这些更改生效。',
         'unallocated_deleted' => '已删除 <code>:ip</code> 的所有未分配端口。',
     ],

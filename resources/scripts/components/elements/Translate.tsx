63f33c7032acc33bb0f43f1efd6fe12066ec43ab
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-02-16 09:38:32 +0530

    update i18n and inputs

--- resources/scripts/components/elements/Translate.tsx
+++ resources/scripts/components/elements/Translate.tsx
@@ -1,6 +1,6 @@
-import { Trans, TransProps, useTranslation } from 'react-i18next';
+import { Trans, TransSelectorProps, useTranslation } from 'react-i18next';
 
-type Props = Omit<TransProps, 't'>;
+type Props = Omit<TransSelectorProps<any, any>, 't'>;
 
 export default ({ ns, children, ...props }: Props) => {
     const { t } = useTranslation(ns);

4fafe76dab3e79ab625f31a4fb3285d9cbfceb40
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-03-14 01:09:10 +0530

    New translations files.php (Kannada)

--- resources/lang/kn/server/files.php
+++ resources/lang/kn/server/files.php
@@ -10,6 +10,11 @@ return [
     'archive' => 'ಆರ್ಕೈವ್',
     'delete' => 'ಅಳಿಸಿ',
     'create-directory' => 'ಡೈರೆಕ್ಟರಿ ರಚಿಸಿ',
+    'file-name-label' => 'File Name',
+    'file-name-description' => 'Enter the name that this file should be saved as.',
+    'create-file' => 'Create File',
+    'file-mode-label' => 'File Mode',
+    'update' => 'Update',
     'url-download' => [
         'button' => 'URL Download',
         'url-label' => 'File URL',

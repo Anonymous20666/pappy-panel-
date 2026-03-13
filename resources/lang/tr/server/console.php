78d361d19d46d5b77c44ebdb347026a99cca2724
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-03-14 01:10:00 +0530

    New translations console.php (Turkish)

--- resources/lang/tr/server/console.php
+++ resources/lang/tr/server/console.php
@@ -6,4 +6,9 @@ return [
     'server-installing' => 'Bu sunucu şu anda kurulum işlemini çalıştırıyor ve çoğu eylem kullanılamıyor.',
     'server-transferring' => 'Bu sunucu şu anda başka bir düğüme transfer ediliyor ve tüm eylemler kullanılamıyor.',
     'run-command' => 'Bir komut yazın...',
+    'connection-trouble' => "We're having trouble connecting to your server, please wait...",
+    'uptime' => 'Uptime',
+    'network-inbound' => 'Network (Inbound)',
+    'network-outbound' => 'Network (Outbound)',
+    'offline' => 'Offline',
 ];

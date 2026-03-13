166d0fbb23a53783b5ffc91e9776b7360bf2ca88
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-03-14 01:10:01 +0530

    New translations schedules.php (Turkish)

--- resources/lang/tr/server/schedules.php
+++ resources/lang/tr/server/schedules.php
@@ -4,4 +4,9 @@ return [
     'title' => 'Zamanlamalar',
     'no-schedules' => 'Bu sunucu için yapılandırılmış zamanlama yok.',
     'create-schedule' => 'Zamanlama Oluştur',
+    'last-run-at' => 'Last run at: :time',
+    'never' => 'never',
+    'active' => 'Active',
+    'inactive' => 'Inactive',
+    'processing' => 'Processing',
 ];

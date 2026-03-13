7ca15761a639990d4308c1d6befd3a0dc56be5e8
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-03-14 01:08:54 +0530

    New translations console.php (Spanish)

--- resources/lang/es/server/console.php
+++ resources/lang/es/server/console.php
@@ -6,4 +6,9 @@ return [
     'server-installing' => 'Este servidor se encuentra ejecutando su proceso de instalación y la mayoría de las acciones no están disponibles.',
     'server-transferring' => 'Este servidor está siendo transferido a otro nodo y todas las acciones están deshabilitadas.',
     'run-command' => 'Escribe un comando...',
+    'connection-trouble' => "We're having trouble connecting to your server, please wait...",
+    'uptime' => 'Uptime',
+    'network-inbound' => 'Network (Inbound)',
+    'network-outbound' => 'Network (Outbound)',
+    'offline' => 'Offline',
 ];

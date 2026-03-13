1e8ec2996684fa0096abaf045bef7d347c841a95
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-03-14 01:09:25 +0530

    New translations strings.php (Arabic)

--- resources/lang/ar/strings.php
+++ resources/lang/ar/strings.php
@@ -93,4 +93,12 @@ return [
     'save' => 'حفظ',
     'copyright' => '&copy; :year Reviactyl™ Software',
     'local_network' => 'الشبكة المحلية',
+    'server-status' => 'Server Status',
+    'loading-server-status' => 'Loading server status...',
+    'no-description' => 'No description provided.',
+    'resource-usage' => 'Resource Usage',
+    'server-not-found' => 'Server not found.',
+    'copied-value-to-clipboard' => 'Copied ":text" to clipboard.',
+    'copied-to-clipboard' => 'Copied text to clipboard.',
+    'copied' => 'Copied!',
 ];

6bfd63c2f78b549f1dcd29d51de7dc0615713c38
jasonzli <jasonzxl@icloud.com>
2026-03-13 00:07:32 +0000

    fix: backup context menu flash + add right click

--- resources/scripts/components/elements/DropdownMenu.tsx
+++ resources/scripts/components/elements/DropdownMenu.tsx
@@ -38,9 +38,11 @@ class DropdownMenu extends React.PureComponent<Props, State> {
         const menu = this.menu.current;
 
         if (this.state.visible && !prevState.visible && menu) {
-            document.addEventListener('click', this.windowListener);
-            document.addEventListener('contextmenu', this.contextMenuListener);
             menu.style.left = `${Math.round(this.state.posX - menu.clientWidth)}px`;
+            setTimeout(() => {
+                document.addEventListener('click', this.windowListener);
+                document.addEventListener('contextmenu', this.contextMenuListener);
+            }, 0);
         }
 
         if (!this.state.visible && prevState.visible) {

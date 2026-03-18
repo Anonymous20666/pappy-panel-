cc1ade9a64b478c0c677f62ecbdaeb545848b1ae
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-03-18 08:38:26 +0530

    Re-register service providers in providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,30 @@
 <?php
+
+use App\Providers\ActivityLogServiceProvider;
 use App\Providers\AppServiceProvider;
+use App\Providers\AuthServiceProvider;
+use App\Providers\BackupsServiceProvider;
+use App\Providers\BladeServiceProvider;
+use App\Providers\EventServiceProvider;
+use App\Providers\HashidsServiceProvider;
+use App\Providers\Filament\AdminPanelProvider;
+use App\Providers\RouteServiceProvider;
+use App\Providers\RepositoryServiceProvider;
+use App\Providers\ViewComposerServiceProvider;
+use Prologue\Alerts\AlertsServiceProvider;
+
 return [
+    ActivityLogServiceProvider::class,
     AppServiceProvider::class,
+    AuthServiceProvider::class,
+    BackupsServiceProvider::class,
+    BladeServiceProvider::class,
+    EventServiceProvider::class,
+    HashidsServiceProvider::class,
+    AdminPanelProvider::class,
+    RouteServiceProvider::class,
+    RepositoryServiceProvider::class,
+    ViewComposerServiceProvider::class,
+
+    AlertsServiceProvider::class,
 ];

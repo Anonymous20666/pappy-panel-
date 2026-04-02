2f40a2165763a213eede4fcc9d4f58d183ae3a9b
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-04-02 16:46:11 +0530

    use DatabaseTruncation instead of RefreshDatabase
    (!again)

--- tests/Integration/IntegrationTestCase.php
+++ tests/Integration/IntegrationTestCase.php
@@ -7,7 +7,7 @@ use Carbon\CarbonImmutable;
 use Carbon\CarbonInterface;
 use App\Events\ActivityLogged;
 use Illuminate\Support\Facades\Event;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTruncation;
 use Tests\Assertions\AssertsActivityLogged;
 use Tests\Traits\Integration\CreatesTestModels;
 use App\Transformers\Api\Application\BaseTransformer;
@@ -16,7 +16,7 @@ abstract class IntegrationTestCase extends TestCase
 {
     use CreatesTestModels;
     use AssertsActivityLogged;
-    use RefreshDatabase;
+    use DatabaseTruncation;
 
     protected $defaultHeaders = [
         'Accept' => 'application/json',
@@ -37,5 +37,15 @@ abstract class IntegrationTestCase extends TestCase
         return CarbonImmutable::createFromFormat(CarbonInterface::DEFAULT_TO_STRING_FORMAT, $timestamp)
             ->setTimezone(BaseTransformer::RESPONSE_TIMEZONE)
             ->toAtomString();
+    }
+
+    /**
+     * Return the database connections that should be wrapped in a transaction for each test.
+     *
+     * @return array
+     */
+    protected function connectionsToTransact()
+    {
+        return [DB::getDriverName()];
     }
 }

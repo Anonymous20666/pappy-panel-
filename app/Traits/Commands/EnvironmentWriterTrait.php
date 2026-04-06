d60418132ae3de707ef6376737884d8a76b1ffde
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-04-06 17:44:52 +0530

    refactor EnvironmentWriterTrait

--- app/Traits/Commands/EnvironmentWriterTrait.php
+++ app/Traits/Commands/EnvironmentWriterTrait.php
@@ -3,27 +3,10 @@
 namespace App\Traits\Commands;
 
 use App\Exceptions\PanelException;
+use Illuminate\Support\Env;
 
 trait EnvironmentWriterTrait
 {
-    /**
-     * Escapes an environment value by looking for any characters that could
-     * reasonably cause environment parsing issues. Those values are then wrapped
-     * in quotes before being returned.
-     */
-    public function escapeEnvironmentValue(?string $value): string
-    {
-        if (is_null($value)) {
-            return '';
-        }
-
-        if (! preg_match('/^\"(.*)\"$/', $value) && preg_match('/([^\w.\-+\/])+/', $value)) {
-            return sprintf('"%s"', addslashes($value));
-        }
-
-        return $value;
-    }
-
     /**
      * Update the .env file for the application using the passed in values.
      *
@@ -36,18 +19,9 @@ trait EnvironmentWriterTrait
             throw new PanelException('Cannot locate .env file, was this software installed correctly?');
         }
 
-        $saveContents = file_get_contents($path);
-        collect($values)->each(function ($value, $key) use (&$saveContents) {
-            $key = strtoupper($key);
-            $saveValue = sprintf('%s=%s', $key, $this->escapeEnvironmentValue($value));
+        Env::writeVariables($values, $path, true);
 
-            if (preg_match_all('/^'.$key.'=(.*)$/m', $saveContents) < 1) {
-                $saveContents = $saveContents.PHP_EOL.$saveValue;
-            } else {
-                $saveContents = preg_replace('/^'.$key.'=(.*)$/m', $saveValue, $saveContents);
-            }
-        });
-
-        file_put_contents($path, $saveContents);
+        // clear
+        Artisan::call('config:clear');
     }
 }
